a12554f8f1bd1399d268c982916521f2ba5262d2
Camillo Camarda <ccamarda@h-art.com>
2014-07-16 14:21:02 +0200

    StrinUtils test

--- tests/unit/Utility/StringUtilsTest.php
+++ tests/unit/Utility/StringUtilsTest.php
@@ -1,10 +1,12 @@
 <?php
 namespace Utility;
 
+use Hart\Utility\StringUtils;
 
 class StringUtilsTest extends \Codeception\TestCase\Test
 {
    use \Codeception\Specify; 
+
    /**
     * @var \UnitTester
     */
@@ -19,14 +21,58 @@ class StringUtilsTest extends \Codeception\TestCase\Test
     }
 
     // tests
-    public function testMe()
+    public function test_it_removes_emoji_from_a_string()
     {
-        $this->specify("pippo", function() {
-            $this->assertTrue(true);
-            $this->assertTrue(false);
+        $this->specify("Remove emoji from string", function() {
+            $test_characters = array(
+                "🍹",
+                "🚬",
+                "🍀",
+                "🎸",
+                "🐨",
+                "🍓"
+            );
+
+            foreach($test_characters as $char)
+            {
+                $this->assertEquals("" , StringUtils::removeEmoji($char) );    
+            }
+            
+        });
+    }
+
+    public function test_it_replaces_accented_characters_from_a_string()
+    {
+
+        $this->specify("Accents", function() {
+            $this->assertEquals("aeeiouU" , StringUtils::replaceAccentedCharacters("àèéìòùÛ") );    
         });
 
 
+
+    }
+
+    public function test_it_sluggifies_a_string()
+    {
+        $this->specify("Empty string", function() {
+            $this->assertEquals("" , StringUtils::sluggify("") );    
+        });
+
+        $this->specify("Spaces to dashes", function() {
+            $this->assertEquals("hello-world" , StringUtils::sluggify("hello world") );    
+        });
+
+        $this->specify("Accents", function() {
+            $this->assertEquals("url-with-accents" , StringUtils::sluggify("Ûrl with àccents") );    
+        });
+
+        $this->specify("Uppercase to lowercase", function() {
+            $this->assertEquals("hello-world" , StringUtils::sluggify("HeLLo WoRlD") );    
+        });
+
+        $this->specify("Initial spaces", function() {
+            $this->assertEquals("this-is-a-not-trimmed-string" , StringUtils::sluggify("  this is a not trimmed string") );    
+        });
 
     }
 
